993fba967291af408c6054b25028902a28563b08
nwHanni <147682661+nwHanni@users.noreply.github.com>
2023-10-31 09:52:46 -0500

    Add files via upload

---
+++ proyWeb/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRUEBA</title>
+</head>
+<body>
+    <?php 
+     echo "Danny Almeida <br>";
+     echo "Pagina de prueba <br>";
+     echo "BLACKPINK: <br>";
+    ?>
+</body>
+<center>
+    
+<img src="photos/Black_Pink.webp" alt=""></center>
+</html>
